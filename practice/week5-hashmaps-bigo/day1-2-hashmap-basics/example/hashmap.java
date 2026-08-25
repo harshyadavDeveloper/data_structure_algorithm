9a497ed558f53a8ad7f258360e29af374e5adee7
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-25 11:41:47 +0530

    added two sum example

--- practice/week5-hashmaps-bigo/day1-2-hashmap-basics/example/hashmap.java
+++ practice/week5-hashmaps-bigo/day1-2-hashmap-basics/example/hashmap.java
@@ -21,8 +21,10 @@ class Main {
         // System.out.println(fruits + map.get(fruits));
         // }
         frequency();
+        twoSum();
     }
 
+    // print chars with the freq using a hashmap
     public static void frequency() {
         String word = "mississippi";
         HashMap<Character, Integer> freq = new HashMap<>();
@@ -37,5 +39,21 @@ class Main {
             freq.put(c, freq.getOrDefault(c, 0) + 1); // cleaner and shorter method to do the same
         }
         System.out.println(freq);
+    }
+
+    // two sum using hashmap
+    public static void twoSum() {
+        int[] arr = { 2, 7, 11, 15 };
+        int target = 9;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+            if (hashMap.containsKey(complement)) {
+                System.out.println(complement + " + " + arr[i] + " makes the sum " + target);
+                break;
+            }
+            hashMap.put(arr[i], i);
+        }
     }
 }
