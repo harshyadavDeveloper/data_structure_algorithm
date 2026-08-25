24a8369aa6f9a69e2ab1bd8f5c520df62f03970a
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-25 10:44:45 +0530

    added frequency func using hashmap

--- practice/week5-hashmaps-bigo/day1-2-hashmap-basics/example/hashmap.java
+++ practice/week5-hashmaps-bigo/day1-2-hashmap-basics/example/hashmap.java
@@ -2,23 +2,39 @@ import java.util.HashMap;
 
 class Main {
     public static void main(String[] args) {
-        HashMap<String, Integer> map = new HashMap<>();
+        // HashMap<String, Integer> map = new HashMap<>();
 
-        map.put("Apples", 3);
-        map.put("Bananas", 5);
-        map.put("Guvavas", 2);
-        map.put("oranges", 6);
-        map.put("Peaches", 4);
-        map.put("Bananas", 2); // this replaces the old values
+        // map.put("Apples", 3);
+        // map.put("Bananas", 5);
+        // map.put("Guvavas", 2);
+        // map.put("oranges", 6);
+        // map.put("Peaches", 4);
+        // map.put("Bananas", 2); // this replaces the old values
 
-        int val = map.get("Bananas"); // gets the value stored against it
-        boolean isExists = map.containsKey("Tomatoes"); // tells true or false
-        System.out.println("Bananas are: " + val);
-        System.out.println("Does tomatoes exists: " + isExists);
-        System.out.println(map);
+        // int val = map.get("Bananas"); // gets the value stored against it
+        // boolean isExists = map.containsKey("Tomatoes"); // tells true or false
+        // System.out.println("Bananas are: " + val);
+        // System.out.println("Does tomatoes exists: " + isExists);
+        // System.out.println(map);
 
-        for (String fruits : map.keySet()) {
-            System.out.println(fruits + map.get(fruits));
+        // for (String fruits : map.keySet()) {
+        // System.out.println(fruits + map.get(fruits));
+        // }
+        frequency();
+    }
+
+    public static void frequency() {
+        String word = "mississippi";
+        HashMap<Character, Integer> freq = new HashMap<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (freq.containsKey(c)) {
+                freq.put(c, freq.get(c) + 1);
+            } else {
+                freq.put(c, 1);
+            }
         }
+        System.out.println(freq);
     }
 }
