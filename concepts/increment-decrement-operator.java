453d1efdb01018488fafcffa009d9a806527d1bb
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-26 14:43:58 +0530

    solved and executed inrement/decrement problems

--- concepts/increment-decrement-operator.java
+++ concepts/increment-decrement-operator.java
@@ -23,13 +23,49 @@ class Main {
 
         // }
 
-        int[] arr = { 23, 55, 99 };
-        int i = 0;
-        arr[++i] = 30; // first i becomes 1 then arr becomes {23,30,99}
-        System.out.println(i); // prints 1
-        for(int num: arr){
-            System.out.println(num);
-        }
+        // int[] arr = { 23, 55, 99 };
+        // int i = 0;
+        // arr[++i] = 30; // first i becomes 1 then arr becomes {23,30,99}
+        // System.out.println(i); // prints 1
+        // for(int num: arr){
+        // System.out.println(num);
+        // }
+
+        // int x = 4;
+        // int y = x++ + 2;
+        // System.out.println(x + " " + y); // y is 6 and x is 5
+
+        // int x = 4;
+        // int y = ++x + 2;
+        // System.out.println(x + " " + y); // x is 5 and y is 7
+
+        // does changing i++ to ++i here change the final array? Reason about it before
+        // testing.
+        // int i = 0;
+        // int[] arr = new int[5];
+        // while (i < 5) {
+        // arr[i] = i * 10;
+        // ++i;
+        // } // no it will not change output of the array because increment happens at
+        // the end of the loop meaning when it uses the i that time increment had not
+        // happened. pre/post does not matter here becasue new value of i obtained by
+        // increment is used in the next round(iteration)
+        // for (int num : arr) {
+        // System.out.println(num);
+        // }
+
+        // int a = 5;
+        // int b = a++ + ++a;
+        // System.out.println(b); // prints 12, note: here what happened was 5 + 7 it
+        // used the values first then
+        // incremented it and then it first incremented the value and then used it
+        // making it 7
+
+        // int i = 3;
+        // System.out.println(i++ + i++ + i); // 3 + 4 + 5 =12
+
+        int i = 1;
+        System.out.println(i++ + " " + i++ + " " + ++i); // 1 2 4
 
     }
 }
