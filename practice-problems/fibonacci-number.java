53fd46494c2e07a8cad67fa3b76bc8782ea08012
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-30 19:05:14 +0530

    added recursion fibonacci in practice problem

--- practice-problems/fibonacci-number.java
+++ practice-problems/fibonacci-number.java
@@ -1,17 +1,34 @@
-class Main{
-    public static void main(String[] args){
-        int end=10;
-        int first=0;
-        int second=1;
+class Main {
+    public static void main(String[] args) {
+        // int end = 10;
+        // int first = 0;
+        // int second = 1;
 
-        for(int i=0;i<end;i++){
-            System.out.print(first+", ");
+        // for (int i = 0; i < end; i++) {
+        // System.out.print(first + ", ");
 
-            int next = first+second;
-            first=second;
-            second=next;
+        // int next = first + second;
+        // first = second;
+        // second = next;
 
+        // }
+        int end = 10;
+
+        for (int i = 0; i < end; i++) {
+            System.out.print(fibonacci(i) + ", ");
         }
+
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
 
     }
 }
